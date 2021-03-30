fd233462b55a57265cd9d8a7a4f6d4f9024dba3c
Piotr Nowak <peterek99914@interia.pl>
2021-03-30 12:17:30 +0200

    Zabezpieczenie, by nie próbować pobrać przyszłej daty

--- Net_Java-2/Program.cs
+++ Net_Java-2/Program.cs
@@ -12,14 +12,43 @@ namespace Net_Java_2
     {
         static void Main(string[] args)
         {
+            var date = DateTime.Today;
+            string day = date.Day.ToString();
+            string month = date.Month.ToString();
+            string year = date.Year.ToString();
             string data="";
             string waluta = "";
+            string s = "Nie mozna podac kursu z dnia, ktory jeszcze nie nastapil";
             Console.WriteLine("Podaj datę, z której chcesz poznać kurs. Datę podaj w formacie rok-msc-dn");
             data = Console.ReadLine();
-            Console.WriteLine("Podaj walutę");
-            waluta = Console.ReadLine();
-            waluta = waluta.ToUpper();
-            wczytaj(data,waluta);
+            string year1 = data.Substring(0, 4);
+            string month1 = data.Substring(5, 2);
+            string day1 = data.Substring(8, 2);
+            int day_1 = int.Parse(day);
+            int month_1 = int.Parse(month);
+            int year_1 = int.Parse(year);
+            int day1_1 = int.Parse(day1);
+            int month1_1 = int.Parse(month1);
+            int year1_1 = int.Parse(year1);
+            if (year1_1 > year_1)
+            {
+                Console.WriteLine(s);
+            }
+            else if (year1_1 == year_1 && month1_1 > month_1)
+            {
+                Console.WriteLine(s);
+            }
+            else if (year1_1 == year_1 && month1_1 == month_1 && day1_1 > day_1)
+            {
+                Console.WriteLine(s);
+            }
+            else
+            {
+                Console.WriteLine("Podaj walutę");
+                waluta = Console.ReadLine();
+                waluta = waluta.ToUpper();
+                wczytaj(data, waluta);
+            }
             Console.Read();
         }
         public static async void wczytaj(string data,string waluta)
